de7fd3d50d5174ff0ddc1212cc7344e7ccc269b7
regisatl <regisattolou21@outlook.fr>
2024-01-03 18:16:26 +0100

    Mise à jour de magic_numbers

--- magic_numbers.py
+++ magic_numbers.py
@@ -5,6 +5,9 @@ val_max = 100
 
 nombreMystere = random.randint(val_min, val_max)
 
+essai = 1 
+score = 100
+
 loop = True
 
 while loop :
@@ -14,8 +17,13 @@ while loop :
 
       if  valeur_int > nombreMystere :
             print("Plus grand que le nombre mystere")
+            essai +=1
+            score -= 10
       elif valeur_int < nombreMystere :
             print("Plus petit que le nombre mystere")
+            essai +=1
+            score -= 10
       else: 
-            print(f"Félicitation à toi tu as trouvé le nombre mystere, le nombre mystère est : {valeur_int} ")
+            print(f"Félicitations à toi tu as trouvé le nombre mystere, le nombre mystère est : {valeur_int} ")
+            print(f"Nombre d'essais : {essai}\nScore : {score}")
             loop = False
