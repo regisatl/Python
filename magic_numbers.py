078c1d25f30ad82a1dbd50ee38a2983479e2567c
regisatl <regisattolou21@outlook.fr>
2024-01-03 19:36:50 +0100

    Mise à jour du jeu dans le fichier magic_numbers

--- magic_numbers.py
+++ magic_numbers.py
@@ -8,6 +8,8 @@ nombreMystere = random.randint(val_min, val_max)
 essai = 1 
 score = 100
 
+NB_CHANCE = 10
+
 loop = True
 
 while loop :
@@ -15,15 +17,19 @@ while loop :
 
       valeur_int = int(valeur_player)
 
-      if  valeur_int > nombreMystere :
-            print("Plus grand que le nombre mystere")
-            essai +=1
-            score -= 10
-      elif valeur_int < nombreMystere :
-            print("Plus petit que le nombre mystere")
-            essai +=1
-            score -= 10
-      else: 
-            print(f"Félicitations à toi tu as trouvé le nombre mystere, le nombre mystère est : {valeur_int} ")
-            print(f"Nombre d'essais : {essai}\nScore : {score}")
+      if essai <= NB_CHANCE:
+            if  valeur_int > nombreMystere :
+                  print("Plus grand que le nombre mystere")
+                  essai +=1
+                  score -= 10
+            elif valeur_int < nombreMystere :
+                  print("Plus petit que le nombre mystere")
+                  essai +=1
+                  score -= 10
+            else: 
+                  print(f"Félicitations à toi tu as trouvé le nombre mystere, le nombre mystère est : {valeur_int} ")
+                  print(f"Nombre d'essais : {essai}\nScore : {score}")
+                  loop = False
+      else:
             loop = False
+            print(f"Désolé vous avez déjà essayer le nombre maximale de tentative réquise, Le nombre mystères est: {nombreMystere}")
