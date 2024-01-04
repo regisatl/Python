0ef5fe57aa61bef54926406362d7344d6711001f
regisatl <regisattolou21@outlook.fr>
2024-01-04 10:37:48 +0100

    Update de py_web

--- project/py_web.py
+++ project/py_web.py
@@ -1,6 +1,7 @@
 
-age = 50
+age = 50 # Définit l'âge de l'utilisateur
 
+# Crée une liste de tuples, chaque tuple contenant un titre, un auteur et un niveau d'âge
 content = [
     ("Rockaby baby", "UNKNOW", 0),
     ("Eignhties Baby", "Jammy Jams", 0),
@@ -19,28 +20,35 @@ content = [
     ("Book of Love", "Analeine Cal y Mayor", 4),
 ]
 
-if 0 <= age <= 5 : # Bébé 0
+# Selon l'âge de l'utilisateur, le code affiche un message de bienvenue approprié et parcourt la liste de contenu
+
+if 0 <= age <= 5 : # Bébé 0  Si l'utilisateur est un bébé (0-5 ans)
       print("Bienvenue dans l'interface Bébé !")
-      for item in content :
-            if item[2] <= 0 :
-                  print(f"{item[0]} de {item[1]}")
-elif 6 <= age <= 12 : # Enfant 1
+
+      for item in content : # Parcourt chaque élément de la liste de contenu
+            if item[2] <= 0 :  # Si le niveau d'âge de l'élément est approprié pour un bébé,
+                  print(f"{item[0]} de {item[1]}") # imprime le titre et l'auteur de l'élément
+
+elif 6 <= age <= 12 : # Enfant 1 Si l'utilisateur est un enfant (6-12 ans)
       print("Bienvenue dans l'interface Enfant !")
-      for item in content :
-            if item[2] <= 1 :
+      for item in content : 
+            if item[2] <= 1 : # Si le niveau d'âge de l'élément est approprié pour un enfant,
                   print(f"{item[0]} de {item[1]}")
-elif 13 <=  age <= 18 : # Adolescent 2
+
+elif 13 <=  age <= 18 : # Adolescent 2 Si l'utilisateur est un adolescent (13-18 ans)
       print("Bienvenue dans l'interface Ado !")
-      for item in content :
-            if item[2] <= 2 :
+      for item in content : 
+            if item[2] <= 2 : # Si le niveau d'âge de l'élément est approprié pour un adolescent,
                   print(f"{item[0]} de {item[1]}")
-elif 19 <= age <= 50 : # Adulte 3
+
+elif 19 <= age <= 50 : # Adulte 3 Si l'utilisateur est un adulte (19-50 ans)
       print("Bienvenue dans l'interface Adulte !")
       for item in content :
-            if item[2] <= 4 :
+            if item[2] <= 4 : # Si le niveau d'âge de l'élément est approprié pour un adulte,
                   print(f"{item[0]} de {item[1]}")
-else : # Sénior
-      print("Bienvenue dans l'interface Sénioir")
+
+else : # Sénior Si l'utilisateur est un senior (plus de 50 ans)
+      print("Bienvenue dans l'interface Sénior")
       for item in content :
-            if item[2] <= 3 :
+            if item[2] <= 3 : # Si le niveau d'âge de l'élément est approprié pour un senior,
                   print(f"{item[0]} de {item[1]}")
