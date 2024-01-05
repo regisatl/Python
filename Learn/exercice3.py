6b98ddb5db17e50066ce48f610323906fc72235a
regisatl <regisattolou21@outlook.fr>
2024-01-05 14:22:01 +0100

    Update exercice3

--- Learn/exercice3.py
+++ Learn/exercice3.py
@@ -1,7 +1,7 @@
 # Initialiser les variables
 sumNombrePositif = 0
-count = 0
-countOver100 = 0
+counter = 0
+counterOver100 = 0
 loop = True 
 
 while loop :
@@ -10,12 +10,12 @@ while loop :
             print("Vous venez d'entrer une valeur négative : Fin du programme")
             loop = False
       sumNombrePositif += valueEnter
-      count += 1
+      counter += 1
       if valueEnter > 100 :
             print("Vous venez d'entrer une valeur supérieure à 100 ")
-            countOver100 += 1
+            counterOver100 += 1
 
 print(f"La somme des nombres positifs est : {sumNombrePositif}")
-print(f"Le nombre total de données est : {count}")
-print(f"Le nombre de données supérieures à 100 est : {countOver100}")
+print(f"Le nombre total de données est : {counter}")
+print(f"Le nombre de données supérieures à 100 est : {counterOver100}")
 
