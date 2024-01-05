0f76eaf58ba1ee0c718f20d5805c6992aabafab6
regisatl <regisattolou21@outlook.fr>
2024-01-05 12:22:13 +0100

    Update et create file

--- Learn/exercice1.py
+++ Learn/exercice1.py
@@ -1,6 +1,9 @@
-loop = True
 
-while loop :
-      for i in range(0, 21) : 
-            print(f"20 premiers termes: {i} " )
-            loop: False
+
+for i in range(0, 21) : 
+      multiplication = i * 7
+      if multiplication % 2 == 0 :
+            print(f"Ce nombre est pair : {multiplication} ")
+      else : 
+            print(f"Ce nombre est impair : {multiplication} **" )
+      
