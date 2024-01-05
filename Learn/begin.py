f5e2f67b902cc0ab21ee9f2d57f3589eb59dffa5
regisatl <regisattolou21@outlook.fr>
2024-01-05 12:08:29 +0100

    ajout de fichier

---
+++ Learn/begin.py
@@ -0,0 +1,16 @@
+x = 2 
+y = 3
+
+print("égalité :", x == y)
+print("inégalité :", x != y)
+print("inférieur ou égal :", x <= y)
+print("supérieur ou égal :", x >= y)
+print("ET :", False and True)
+print("OU :", False or True)
+print("OU exclusif :", False ^ True)
+
+print("*********************************************")
+a = True
+b = True
+
+print("OU Exclusif :", a ^ b)
