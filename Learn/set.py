66210998ef9c51921c3878d24578e11b22b0a485
regisatl <regisattolou21@outlook.fr>
2024-01-06 10:28:49 +0100

    méthode union et intersection

--- Learn/set.py
+++ Learn/set.py
@@ -2,6 +2,14 @@ mySet = {1, 2, 3, 4, 2, 9, 4, 'a', 'b', 'c'}
 
 otherSet = {5, 6, 7, 8, 'c', 'd', 'd'}
 
+mySet.remove(9)
+
 print(mySet)
 
-print(mySet + otherSet)
+unionSet = mySet.union(otherSet)
+
+print(unionSet)
+
+intersectionSet = mySet.intersection(otherSet)
+
+print(intersectionSet)
