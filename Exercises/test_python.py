0cd5f6ab44e27109638b8bbdb88e82beea5b53fc
regisatl <regisattolou21@outlook.fr>
2024-01-06 09:22:53 +0100

    Update python

--- Exercises/test_python.py
+++ Exercises/test_python.py
@@ -1,33 +1,33 @@
-# nom = input("Votre nom svp !")
-# loop  = True
+nom = input("Votre nom svp !")
+loop  = True
 
-# while loop :
-#       if nom : 
-#             print(f"bonjour {nom}, comment allez vous aujourd'hui !")
-#             loop = False
-#       else:
-#             nom = input("Votre nom svp !")
+while loop :
+      if nom : 
+            print(f"bonjour {nom}, comment allez vous aujourd'hui !")
+            loop = False
+      else:
+            nom = input("Votre nom svp !")
 
 # afficher une liste ordonnée de 10 chiffres
 
-# loop = True
+loop = True
 
-# for i in range(10):
-#     print((i +1 )**2)
+for i in range(10):
+    print((i +1 )**2)
 
 # Les lists
 
-# tab = ["Ted", 24, True, range (15)]
+tab = ["Ted", 24, True, range (15)]
 
-# tab = ("Ted", 24, True, range (15))
+tab = ("Ted", 24, True, range (15))
 
-# print(tab)
-# print(tab[3])
+print(tab)
+print(tab[3])
 
-# for i in tab:
-#       if type(i) == str:
-#         print(i.upper())
-#       elif type(i) == int:
-#         print(i ** 2)
-#       else: 
-#         print(i)
+for i in tab:
+      if type(i) == str:
+        print(i.upper())
+      elif type(i) == int:
+        print(i ** 2)
+      else: 
+        print(i)
