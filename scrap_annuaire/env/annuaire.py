af76270efc87d9d9b0a256853cc19b46f3a9f251
regisatl <regisattolou21@outlook.fr>
2024-01-08 13:02:30 +0100

    installation des dépendances requests, beautifulSoup et update de pip

--- scrap_annuaire/env/annuaire.py
+++ scrap_annuaire/env/annuaire.py
@@ -5,27 +5,25 @@ import re
 verifyGetRequest = requests.get("https://www.barreaudenice.com/annuaire/avocats/?fwp_paged=1")
 print(verifyGetRequest.status_code)
 
-def getAllPagesScraping() :
+def getAllPagesScraping():
       
       urls = []
       pageNumber = 1
       
-      for i in range (107) : 
-            i = f"https://www.barreaudenice.com/annuaire/avocats/?fwp_paged={pageNumber} "
+      for i in range (107): 
+            i = f"https://www.barreaudenice.com/annuaire/avocats/?fwp_paged={pageNumber}"
             pageNumber += 1
             urls.append(i)      
             return urls
-      
-getAllPagesScraping()
 
-def parseAttorney(url) :
+def parseAttorney(url):
       
       request = requests.get(url)
       soup = BeautifulSoup(request.content, "html.parser")
 
       attorneys = soup.find_all("div", class_ ="callout secondary annuaire-single")
       
-      for attorney in attorneys :
+      for attorney in attorneys:
             try :
                   nameAttorney = attorney.find('h3').text.strip()
             except : 
@@ -47,17 +45,17 @@ def parseAttorney(url) :
       
             wayStockData = r"C:\Users\Régis.Attolou\Documents\Github\Python\scrap_annuaire\annuaire_attorney.txt"
             with open(wayStockData, "a") as f :
-                  f.write(f" {nameAttorney}\n")
-                  f.write(f" {adressFinalAttorney}\n")
-                  f.write(f" {telAttorney}\n")
-                  f.write(f" {emailAttorney}\n\n")
+                  f.write(f"{nameAttorney}\n")
+                  f.write(f"{adressFinalAttorney}\n")
+                  f.write(f"{telAttorney}\n")
+                  f.write(f"{emailAttorney}\n\n")
             
 
 def parseAllAttorneys() :
       
       pages = getAllPagesScraping()
-      for page in pages :
-            parseAttorney(url = page)
+      for page in pages:
+            parseAttorney(url=page)
             print(f"On scrape {page} ")
             
 parseAllAttorneys()
