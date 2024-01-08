809e42262f6ebf09313b64cd468f2e925c62d6d6
regisatl <regisattolou21@outlook.fr>
2024-01-08 11:03:28 +0100

    creation de la fonction permettant de récupérer les urls des différentes pages

--- scrap_annuaire/env/annuaire.py
+++ scrap_annuaire/env/annuaire.py
@@ -1,2 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+
+verifyGetRequest = requests.get("https://www.barreaudenice.com/annuaire/avocats/?fwp_paged=1")
+print(verifyGetRequest.status_code)
+
+def getAllPagesScraping():
+      
+      urls = []
+      
+      pageNumber = 1
+      
+      for i in range (107) : 
+            i = f"https://www.barreaudenice.com/annuaire/avocats/?fwp_paged={pageNumber} "
+            pageNumber += 1
+            urls.append(i)      
+
+            return urls
+      
+getAllPagesScraping()
