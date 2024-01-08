ca02f6793f40d490953ac59f40f07514deb82431
regisatl <regisattolou21@outlook.fr>
2024-01-08 12:22:09 +0100

    scraping d'une page

--- scrap_annuaire/env/annuaire.py
+++ scrap_annuaire/env/annuaire.py
@@ -1,5 +1,6 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+import re
 
 verifyGetRequest = requests.get("https://www.barreaudenice.com/annuaire/avocats/?fwp_paged=1")
 print(verifyGetRequest.status_code)
@@ -17,15 +18,31 @@ def getAllPagesScraping() :
       
 getAllPagesScraping()
 
-def parseAttorney() :
+def parseAttorney(url) :
       request = requests.get("https://www.barreaudenice.com/annuaire/avocats/?fwp_paged=1")
       soup = BeautifulSoup(request.content, "html.parser")
 
       attorneys = soup.find_all("div", class_ ="callout secondary annuaire-single")
       
       for attorney in attorneys :
-            name = attorney.find('h3').text.strip()
+            nameAttorney = attorney.find('h3').text.strip()
             adressAttorney = attorney.find('span', class_ = 'adresse').text.strip()
-            print(adressAttorney)
-      
-parseAttorney()
+            adressFinalAttorney = re.sub(r"\s*", " ", adressAttorney)
+            telAttorney = attorney.find('span', class_ = 'telephone').text.strip()
+            emailAttorney = attorney.find('span', class_ = 'email').a.text.strip()
+            
+            wayStockData = r"C:\Users\Régis.Attolou\Documents\Github\Python\scrap_annuaire\annuaire_attorney.txt"
+            with open(wayStockData, "a") as f :
+                  f.write(f" {nameAttorney}\n")
+                  f.write(f" {adressFinalAttorney}\n")
+                  f.write(f" {telAttorney}\n")
+                  f.write(f" {emailAttorney}\n\n")
+            
+
+def parseAllAttorneys() :
+      pages = getAllPagesScraping()
+      for page in pages :
+            parseAttorney(url = page)
+            print(f"On scrape {page} ")
+            
+parseAllAttorneys()
