39bed37033de219da0ff48c7cb509b4706614b84
regisatl <regisattolou21@outlook.fr>
2024-01-09 15:55:39 +0100

    update complete of file

--- scrap_quelleenergie/legalPlace.py
+++ scrap_quelleenergie/legalPlace.py
@@ -2,46 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-urls = []
-
 def extractUrlsData():
+      urls = []
       request = requests.get("https://www.legalplace.fr/")
       soup = BeautifulSoup(request.content, "html.parser")
-      
       dataText = soup.find_all("a", href=True)
-      
-      # Afficher les liens href             
+    
+    # Récupérer les liens href
       for link in dataText:
-            baseUrl ="https://www.legalplace.fr"
-            urlsInit = []
-            urlsInit.append(link['href'])
-            for url in urlsInit:
-                  if url.startswith("https://"):
-                       print(url)
-                  elif url.startswith("/"):
-                        urLink = f"{baseUrl}" + url
-                        print(urLink)
-                  elif url.startswith("#"):
-                        pass
+            baseUrl = "https://www.legalplace.fr"
+            url = link['href']
+            if url.startswith("https://"):
+                  urls.append(url)
+            elif url.startswith("/"):
+                  urLink = f"{baseUrl}{url}"
+                  urls.append(urLink)
+            elif url.startswith("#"):
+                  pass
+      return urls
 
-extractUrlsData()
+# Appel de la fonction et affichage des URLs
+extracted_urls = extractUrlsData()
 
-# def getAllPagesScraping(link):
-#       requete = requests.get(link)
-#       soup = BeautifulSoup(requete.content, "html.parser")
-#       text = soup.get_text()
-#       finalText = re.sub(r"\s*", " ", text)
+
+def getAllPagesScraping(link):
+      requete = requests.get(link)
+      soup = BeautifulSoup(requete.content, "html.parser")
+      text = soup.get_text()
+      finalText = re.sub(r"\s*", " ", text)
       
-#       wayStockData = r"C:\Users\Régis.Attolou\Documents\Github\Python\scrap_quelleenergie/flora.txt"
-#       with open(wayStockData, "a", encoding="utf-8") as f :
-#             f.write(f"{link}\n") 
-#             f.write(f"{finalText}\n\n")
+      wayStockData = r"C:\Users\Régis.Attolou\Documents\Github\Python\scrap_quelleenergie/legalPlace.txt"
+      with open(wayStockData, "a", encoding="utf-8") as f :
+            f.write(f"{link}\n") 
+            f.write(f"{finalText}\n\n")
 
 
-# def getAllDataPages() :
-#       pages = extractUrlsData()
-#       for page in pages:
-#             getAllPagesScraping(link=page)
-#             print(f"On scrappe : {page} ")             
-# getAllDataPages()
+def getAllDataPages() :
+      pages = extracted_urls
+      for page in pages:
+            getAllPagesScraping(link=page)
+            print(f"On scrappe : {page} ")             
+getAllDataPages()
 
