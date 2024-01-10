c01392b1d7540e0d6e6200307282273e19e446b5
regisatl <regisattolou21@outlook.fr>
2024-01-10 11:11:59 +0100

    update file and add code

--- scrap_quelleenergie/scrapingPages.py
+++ scrap_quelleenergie/scrapingPages.py
@@ -5,39 +5,47 @@ import re
 urls = []
 
 def extractUrlsData():
-      request = requests.get("https://www.quelleenergie.fr/")
-      soup = BeautifulSoup(request.content, "html.parser")
+      requete = requests.get("https://fr.luko.eu/")
+      soup = BeautifulSoup(requete.content, "html.parser")
       
-      dataText = soup.find_all("a")
+      dataLink = soup.find_all("a", href=True)
       
-      for i in dataText:
-            base_url = "https://www.quelleenergie.fr"
-            link_suffixe = i["href"]
-            final_url = ""
-            if link_suffixe.startswith("https://"):
-                  final_url = link_suffixe
-            elif link_suffixe.startswith("/"):
-                  final_url = base_url + link_suffixe
-                  if not final_url in urls:
-                        urls.append(final_url)
+      for link in dataLink:
+            baseUrl = "https://fr.luko.eu/"
+            linkSuffixe = link['href']
+            tempUrl = ""
+            if linkSuffixe.startswith("https://"):
+                  tempUrl = linkSuffixe
+                  print(tempUrl)
+            elif linkSuffixe.startswith(r'/[a-zA-Z0-9]+'):
+                  tempUrl = baseUrl + linkSuffixe
+                  print(tempUrl)
+            elif linkSuffixe.startswith("#"):
+                  return None
+            if not tempUrl in urls:
+                  urls.append(tempUrl)
       return urls
 
-def getAllPagesScraping(link):
-      requete = requests.get(link)
-      soup = BeautifulSoup(requete.content, "html.parser")
-      text = soup.get_text()
-      finalText = re.sub(r"\s*", " ", text)
+extractData = extractUrlsData()
+for data in extractData:
+      print(data)
+
+# def getAllPagesScraping(link):
+#       requete = requests.get(link)
+#       soup = BeautifulSoup(requete.content, "html.parser")
+#       text = soup.get_text()
+#       finalText = re.sub(r"\s*", " ", text)
       
-      wayStockData = r"C:\Users\Régis.Attolou\Documents\Github\Python\scrap_quelleenergie/quelleenergie.txt"
-      with open(wayStockData, "a", encoding="utf-8") as f :
-            f.write(f"{link}\n") 
-            f.write(f"{finalText}\n\n")
+#       wayStockData = r"C:\Users\Régis.Attolou\Documents\Github\Python\scrap_quelleenergie/latribune.txt"
+#       with open(wayStockData, "a", encoding="utf-8") as f :
+#             f.write(f"{link}\n") 
+#             f.write(f"{finalText}\n\n")
 
 
-def getAllDataPages() :
-      pages = extractUrlsData()
-      for page in pages:
-            getAllPagesScraping(link=page)
-            print(f"On scrappe : {page} ")             
-getAllDataPages()
+# def getAllDataPages() :
+#       pages = extractUrlsData()
+#       for page in pages:
+#             getAllPagesScraping(link=page)
+#             print(f"On scrappe : {page} ")             
+# getAllDataPages()
 
