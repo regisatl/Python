6a5b5cd56bbd24e79044789a27832c98a0abe452
regisatl <regisattolou21@outlook.fr>
2024-01-10 19:56:54 +0100

    update

--- scrap_quelleenergie/quelleenergie.py
+++ scrap_quelleenergie/quelleenergie.py
@@ -5,39 +5,40 @@ import re
 urls = []
 
 def extractUrlsData():
-      request = requests.get("https://www.latribune.fr/")
+      request = requests.get("https://www.statista.com/")
       soup = BeautifulSoup(request.content, "html.parser")
       
-      dataText = soup.find_all("a")
+      dataText = soup.find_all("a", href=True)
       
-      for i in dataText:
-            if 'href' in i.attrs:  # Vérifiez si l'attribut 'href' existe
-                  base_url = "https://www.latribune.fr"
-                  link_suffixe = i["href"]
-                  final_url = ""
-                  if link_suffixe.startswith("http://") or link_suffixe.startswith("https://"):
-                        final_url = link_suffixe
-                  elif link_suffixe.startswith("www"):
-                        final_url = "https://" + link_suffixe
-                  elif link_suffixe.startswith(r"/[a-zA-Z0-9]+"):
-                        final_url = base_url + link_suffixe
-                        if not final_url in urls:
-                              urls.append(final_url)
-      return urls
+      for tempLink in dataText:
+            baseUrl = "https://www.statista.com"
+            url = tempLink['href']
+            if url.startswith("https://") or url.startswith("http://"):
+                  if not url in urls:
+                        urls.append(url)
+            elif url.startswith(r"/[w]+"):
+                  urLink = f"{baseUrl}{url}"
+                  if not url in urls:
+                        urls.append(urLink)
+            elif url.startswith("#"):
+                  pass
+      return urls  
+
+extractData = extractUrlsData()
 
 def getAllPagesScraping(link):
       requete = requests.get(link)
       soup = BeautifulSoup(requete.content, "html.parser")
       text = soup.get_text()
-      finalText = re.sub(r"\s*", " ", text)
+      finalText = re.sub(r"\s+", " ", text)
 
-      with open('latribune.txt', "a", encoding="utf-8") as f :
+      with open('statista.txt', "a", encoding="utf-8") as f :
             f.write(f"{link}\n") 
             f.write(f"{finalText}\n\n")
 
 
 def getAllDataPages() :
-      pages = extractUrlsData()
+      pages = extractData
       for page in pages:
             getAllPagesScraping(link=page)
             print(f"On scrappe : {page} ")             
