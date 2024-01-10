1f26384ca4da447a231a5e10c32f25aa54f2a292
regisatl <regisattolou21@outlook.fr>
2024-01-10 18:39:54 +0100

    add

--- scrap_quelleenergie/scraping.py
+++ scrap_quelleenergie/scraping.py
@@ -56,7 +56,7 @@ def scrape_site(url):
     base_url = url
     valid_links = [link for link in links if validate_link(link, base_url) and link is not None]
     
-    with open('gea-assurances.txt', 'w', encoding="utf-8") as f:
+    with open('blog-weproc.txt', 'w', encoding="utf-8") as f:
         for link in valid_links:
             try:
                  data = extract_data(link)
@@ -82,4 +82,4 @@ def scrape_site(url):
             except AttributeError:
                 print("Scrapping échoué") # Ajout d'un message d'échec
 
-scrape_site('https://gea-assurances.com')
+scrape_site('https://blog.weproc.com/')
