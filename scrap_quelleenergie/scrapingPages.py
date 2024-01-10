8def869c18a4cfa7f6b852bbd04897bdf7474470
regisatl <regisattolou21@outlook.fr>
2024-01-10 10:11:32 +0100

    update file scraping

--- scrap_quelleenergie/scrapingPages.py
+++ scrap_quelleenergie/scrapingPages.py
@@ -2,5 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-verifyStatutsCode = requests.get("https://www.latribune.fr/")
-print(verifyStatutsCode.status_code)
+urls = []
+
+def extractUrlsData():
+      request = requests.get("https://www.quelleenergie.fr/")
+      soup = BeautifulSoup(request.content, "html.parser")
+      
+      dataText = soup.find_all("a")
+      
+      for i in dataText:
+            base_url = "https://www.quelleenergie.fr"
+            link_suffixe = i["href"]
+            final_url = ""
+            if link_suffixe.startswith("https://"):
+                  final_url = link_suffixe
+            elif link_suffixe.startswith("/"):
+                  final_url = base_url + link_suffixe
+                  if not final_url in urls:
+                        urls.append(final_url)
+      return urls
+
+def getAllPagesScraping(link):
+      requete = requests.get(link)
+      soup = BeautifulSoup(requete.content, "html.parser")
+      text = soup.get_text()
+      finalText = re.sub(r"\s*", " ", text)
+      
+      wayStockData = r"C:\Users\Régis.Attolou\Documents\Github\Python\scrap_quelleenergie/quelleenergie.txt"
+      with open(wayStockData, "a", encoding="utf-8") as f :
+            f.write(f"{link}\n") 
+            f.write(f"{finalText}\n\n")
+
+
+def getAllDataPages() :
+      pages = extractUrlsData()
+      for page in pages:
+            getAllPagesScraping(link=page)
+            print(f"On scrappe : {page} ")             
+getAllDataPages()
+
