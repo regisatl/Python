2c994bb69c0fefec6e915adc79c5845a287d7ff7
regisatl <regisattolou21@outlook.fr>
2024-01-11 12:04:36 +0100

    add multiple file .txt

--- scrapSite/scraping.py
+++ scrapSite/scraping.py
@@ -5,13 +5,13 @@ import re
 urls = []
 
 def extractUrlsData():
-      request = requests.get("https://www.tsfi.org/fr")
+      request = requests.get("https://www.sfr.fr/")
       soup = BeautifulSoup(request.content, "html.parser")
       
       dataText = soup.find_all("a", href=True)
       
       for tempLink in dataText:
-            baseUrl = "https://www.tsfi.org/fr"
+            baseUrl = "https://www.sfr.fr/"
             url = tempLink['href']
             if url.startswith("https://") or url.startswith("http://"):
                   if not url in urls:
@@ -37,7 +37,7 @@ def getAllPagesScraping(link):
       texte = soup.get_text()
       finalText = re.sub(r"\s+", " ", texte)
 
-      with open('tsfi.txt', "a", encoding="utf-8") as f :
+      with open('sfr.txt', "a", encoding="utf-8") as f :
             f.write(f"Lien: {link}\n") 
             f.write(f"Text sur la page: {finalText}\n\n")
 
