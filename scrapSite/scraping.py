71a64495f83f39bb51c0638ec9511904f1f3fd6c
regisatl <regisattolou21@outlook.fr>
2024-01-11 11:21:26 +0100

    add file journaldugeek.txt

--- scrapSite/scraping.py
+++ scrapSite/scraping.py
@@ -5,13 +5,13 @@ import re
 urls = []
 
 def extractUrlsData():
-      request = requests.get("https://www.igen.fr/")
+      request = requests.get("https://www.journaldugeek.com/")
       soup = BeautifulSoup(request.content, "html.parser")
       
       dataText = soup.find_all("a", href=True)
       
       for tempLink in dataText:
-            baseUrl = "https://www.igen.fr/"
+            baseUrl = "https://www.journaldugeek.com/"
             url = tempLink['href']
             if url.startswith("https://") or url.startswith("http://"):
                   if not url in urls:
@@ -37,7 +37,7 @@ def getAllPagesScraping(link):
       text = soup.get_text()
       finalText = re.sub(r"\s+", " ", text)
 
-      with open('igen.txt', "a", encoding="utf-8") as f :
+      with open('journaldugeek.txt', "a", encoding="utf-8") as f :
             f.write(f"Lien: {link}\n") 
             f.write(f"Text sur la page: {finalText}\n\n")
 
