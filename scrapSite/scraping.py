ea94e2cbab3f2c06026933d0053419b6beebcb0d
regisatl <regisattolou21@outlook.fr>
2024-01-11 11:01:24 +0100

    add file freenews.txt

--- scrapSite/scraping.py
+++ scrapSite/scraping.py
@@ -5,13 +5,13 @@ import re
 urls = []
 
 def extractUrlsData():
-      request = requests.get("https://www.lesnumeriques.com/")
+      request = requests.get("https://www.freenews.fr/")
       soup = BeautifulSoup(request.content, "html.parser")
       
       dataText = soup.find_all("a", href=True)
       
       for tempLink in dataText:
-            baseUrl = "https://www.lesnumeriques.com/"
+            baseUrl = "https://www.freenews.fr/"
             url = tempLink['href']
             if url.startswith("https://") or url.startswith("http://"):
                   if not url in urls:
@@ -32,11 +32,12 @@ extractData = extractUrlsData()
 
 def getAllPagesScraping(link):
       requete = requests.get(link)
-      soup = BeautifulSoup(requete.content, "html.parser")
+      soup = BeautifulSoup(requete.content, features="xml")
+
       text = soup.get_text()
       finalText = re.sub(r"\s+", " ", text)
 
-      with open('lesnumeriques.txt', "a", encoding="utf-8") as f :
+      with open('freenews.txt', "a", encoding="utf-8") as f :
             f.write(f"Lien: {link}\n") 
             f.write(f"Text sur la page: {finalText}\n\n")
 
