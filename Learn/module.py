f1928d093e67fd0ce10bf82c4024d2c04f6143f7
regisatl <regisattolou21@outlook.fr>
2024-01-12 17:13:30 +0100

    update file module, test

--- Learn/module.py
+++ Learn/module.py
@@ -1,18 +1,26 @@
 import math
-nombres = [5, 10, 15, 20, 25]
+import random
 
+nombres = [5, 10, 15, 20, 25]
 def calculer_moyenne(nombres):
       somme = 0
       for nombre in nombres:
             somme += nombre
       return somme / len(nombres)
-      
+
+# rayon = int(input("Veuillez entrer le rayon de votre cône s'il vous plaît : "))
+# hauteur = int(input("Veuillez entrer la hauteur de votre cône s'il vous plaît : "))
 def calculer_volume_cone_droit(rayon, hauteur):
       return math.pi * rayon ** 2 * hauteur
 
-def afficher_pair_impair():
-      for nombre in nombres:
-            if nombre % 2 == 0:
-                  print(f"{nombre} est pair")
-            else:
-                  print(f"{nombre} est impair")
+# nombre_utilisateur = int(input("Veuillez entrer votre nombre s'il vous plaît: "))
+def afficher_pair_impair(nombre_utilisateur):
+      if nombre_utilisateur % 2 == 0:
+            return nombre_utilisateur, 'est pair'
+      else:
+            return nombre_utilisateur, 'est impair'
+            
+start = int(input("Veuillez entrer la valeur de départ s'il vous plaît : "))
+end = int(input("Veuillez entrer la valeur de fin s'il vous plaît : "))
+def nombre_aleatoire(start, end):
+      return random.randint(start, end)
--- Learn/test.py
+++ Learn/test.py
@@ -1,3 +1,9 @@
 import module
 
-print(f"La moyenne est : {module.calculer_moyenne(module.nombres)} ")
+# print(f"La moyenne est : {module.calculer_moyenne(module.nombres)} ")
+
+# print(f"Le volume de votre cône droit est : {module.calculer_volume_cone_droit(module.rayon, module.hauteur)} ")
+
+# print(f"Le nombre que vous avez entrer est: {module.nombre_utilisateur} et {module.afficher_pair_impair(module.nombre_utilisateur)}")
+
+print(f"Les nombres aléatoire que vous avez entre {module.start} et  {module.end} sont : {module.nombre_aleatoire(module.start, module.end)}")
