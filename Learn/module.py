c68df9a599122da2d49e447486ba23f18c330fdb
regisatl <regisattolou21@outlook.fr>
2024-01-12 17:26:28 +0100

    last update module and test

--- Learn/module.py
+++ Learn/module.py
@@ -8,12 +8,20 @@ def calculer_moyenne(nombres):
             somme += nombre
       return somme / len(nombres)
 
-# rayon = int(input("Veuillez entrer le rayon de votre cône s'il vous plaît : "))
-# hauteur = int(input("Veuillez entrer la hauteur de votre cône s'il vous plaît : "))
+rayon = int(input("Veuillez entrer le rayon de votre cône s'il vous plaît : "))
+hauteur = int(input("Veuillez entrer la hauteur de votre cône s'il vous plaît : "))
 def calculer_volume_cone_droit(rayon, hauteur):
       return math.pi * rayon ** 2 * hauteur
 
-# nombre_utilisateur = int(input("Veuillez entrer votre nombre s'il vous plaît: "))
+def combien_fois_entier_positif_divisible_par_deux():
+      nombre = int(input("Veuillez entrer un entier strictement positif : "))
+      nombre_de_diviseurs = 0
+      for i in range(1, nombre):
+            if nombre % i == 0:
+                  nombre_de_diviseurs += 1
+      return nombre_de_diviseurs
+
+nombre_utilisateur = int(input("Veuillez entrer votre nombre s'il vous plaît: "))
 def afficher_pair_impair(nombre_utilisateur):
       if nombre_utilisateur % 2 == 0:
             return nombre_utilisateur, 'est pair'
--- Learn/test.py
+++ Learn/test.py
@@ -1,9 +1,11 @@
 import module
 
-# print(f"La moyenne est : {module.calculer_moyenne(module.nombres)} ")
+print(f"La moyenne est : {module.calculer_moyenne(module.nombres)}")
 
-# print(f"Le volume de votre cône droit est : {module.calculer_volume_cone_droit(module.rayon, module.hauteur)} ")
+print(f"Le volume de votre cône droit est : {module.calculer_volume_cone_droit(module.rayon, module.hauteur)} ")
 
-# print(f"Le nombre que vous avez entrer est: {module.nombre_utilisateur} et {module.afficher_pair_impair(module.nombre_utilisateur)}")
+print(f"Cet entier est divisible par 2 : {module.combien_fois_entier_positif_divisible_par_deux()} fois")
+
+print(f"Le nombre que vous avez entrer est: {module.nombre_utilisateur} et {module.afficher_pair_impair(module.nombre_utilisateur)}")
 
 print(f"Les nombres aléatoire que vous avez entre {module.start} et  {module.end} sont : {module.nombre_aleatoire(module.start, module.end)}")
