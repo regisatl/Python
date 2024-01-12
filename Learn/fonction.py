5ea02c471c0c7a1bf58a3bbc3cd1bdf817d622d7
regisatl <regisattolou21@outlook.fr>
2024-01-12 10:24:45 +0100

    update fonction

--- Learn/fonction.py
+++ Learn/fonction.py
@@ -2,3 +2,11 @@ def year():
       print('Nous somme en 2024')
 
 year()
+
+years = 2024
+def next_year():
+      global years
+      years += 1
+      print(f'Nous sommes maintenant en {years} ')
+
+next_year()
