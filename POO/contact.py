4eb8fe5bc81b8b88387d33d9c24732c92282dafd
regisatl <regisattolou21@outlook.fr>
2024-01-13 12:29:48 +0100

    creation de fichier moduler

---
+++ POO/contact.py
@@ -0,0 +1,8 @@
+
+class Contact:
+      def __init__(self, nom, prenom, telephone, adresse, email):
+            self.nom = nom
+            self.prenom = prenom
+            self.telephone = telephone
+            self.adresse = adresse
+            self.email = email
---
+++ POO/contact_manager.py
@@ -0,0 +1,31 @@
+import contact
+
+class ContactManager(contact.Contact):
+      def __init__(self, nom, prenom, telephone, adresse, email):
+            self.nom = nom
+            self.prenom = prenom
+            self.telephone = telephone
+            self.adresse = adresse
+            self.email = email
+            self.contacts = []
+      def ajouter_contact(self, contact):
+            self.contacts.append(contact)
+      
+      def affiche_contacts(self):
+            for contact in self.contacts:
+                  print(contact.nom, contact.prenom, contact.telephone, contact.adresse, contact.email)
+                  
+      
+      def supprimer_contact(self, contact):
+            self.contacts.remove(contact)
+      
+      def chercher_contact(self, nom,prenom):
+            for contact in self.contacts:
+                  if contact.nom == nom and contact.prenom == prenom:
+                        return contact
+      
+      
+      
+      
+                  
+     
