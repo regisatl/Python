b6d96de476ba65e815ccbe0c87e479d228df2de2
regisatl <regisattolou21@outlook.fr>
2024-01-16 11:13:24 +0100

    mise à jour de scrape.py

--- scrapSite/scrape.py
+++ scrapSite/scrape.py
@@ -3,10 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 
 urls = []
-baseUrl = "https://www.ooinvestir.fr/"
+baseUrl = "https://www.nexity.fr/"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"}
+
 def extractUrlsData():
       try:
-            request = requests.get(baseUrl)
+            request = requests.get(baseUrl, headers=headers)
             request.raise_for_status() 
       except requests.exceptions.HTTPError as errh:
             print(f"HTTP Error: {errh}")
@@ -18,8 +20,8 @@ def extractUrlsData():
             print(f"OOps: Something Else {err}")
 
       if request.status_code == 200:
+            
             soup = BeautifulSoup(request.content, "html.parser")
-      
             dataText = soup.find_all("a", href=True)
       
             for tempLink in dataText:
@@ -38,12 +40,15 @@ def extractUrlsData():
                  elif url.startswith("#"):
                         pass
             return urls  
+      
+      else:
+            print(f"Impossible to scrape this page, its status code is : {request.status_code}")
 
 extractData = extractUrlsData()
 
 def getAllPagesScraping(link):
       try:
-            requete = requests.get(link)
+            requete = requests.get(link, headers=headers)
             requete.raise_for_status() 
       except requests.exceptions.HTTPError as errh:
             print(f"HTTP Error: {errh}")
@@ -52,18 +57,21 @@ def getAllPagesScraping(link):
       except requests.exceptions.Timeout as errt:
             print(f"Timeout Error: {errt}")
       except requests.exceptions.RequestException as err:
-            print(f"OOps: Something Else {err}")
+            print(f"Oups: Something Else {err}")
 
-      if requete.status_code == 200:
-            soup = BeautifulSoup(requete.content, features="xml")
+            if requete.status_code == 200:
 
-            texte = soup.get_text()
-            finalText = re.sub(r"\s+", " ", texte)
+                  textSoup = BeautifulSoup(requete.content, 'html.parser')
+                  text_content = textSoup.get_text()
+                  cleaned_text = re.sub(r'\s+', ' ', text_content)
 
-            with open('data.txt', "a", encoding="utf-8") as f :
-                 f.write(f"Lien: {link}\n") 
-                 f.write(f"Text sur la page: {finalText}\n\n")
-
+                  with open('nexity.txt', "a", encoding="utf-8") as file :
+                        file.write(f"Lien: {link}\n") 
+                        file.write(f"Contenu de la la page: {cleaned_text}\n\n")
+            else: 
+                  print("Requete is not assigned a value.")
+      except UnboundLocalError:
+            print("Requete is not defined in the current local scope.")
 
 def getAllDataPages() :
       pages = extractData
