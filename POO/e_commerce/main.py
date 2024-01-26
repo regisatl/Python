4ab9b5902fea5bc9c3c583d18fb3d9763a19f2c0
regisatl <regisattolou21@outlook.fr>
2024-01-26 16:46:21 +0100

    Ajout de la fonction qui permet de fermer la connexion à la base de données

--- POO/e_commerce/main.py
+++ POO/e_commerce/main.py
@@ -4,14 +4,15 @@ from produit import Produit
 if __name__ == "__main__":  # Si le script est exécuté directement (et non importé).
 
       database = BaseDeDonnees()
+      
       product = tuple(Produit('Pizza', 1000))
       table_name = "produits"
       columns = ('nom', 'prix')
       
       values =product
-      print(product)
       
-      # database.insert_table(table_name, columns, values)
+      database.insert_table(table_name, columns, values)
+      database.close()
       
       
       
